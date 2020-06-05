5aa4ba286ed60ed7d9e608eaa918c155678e8cc3
Aaron Espere <aespere0@gmail.com>
2020-06-05 16:19:17 -0400

    Initialized XCode Project

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SAKUBUN
+//
+//  Created by Aaron Espere on 6/5/20.
+//  Copyright © 2020 Aaron Espere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
